79264f1c63fe7b83c2847caee4183e5311435dc1
Tony <tony.kiplagat@student.moringaschool.com>
2023-09-21 13:51:29 +0300

    Final student teacher 1

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,11 @@
 from user import User
 
 class Student(User):
+    knowledge = []
     
-    def learn(self):
-        pass
+    def __init__(self,first_name,last_name,knowledge=knowledge):
+        super().__init__(first_name,last_name)
+        self.knowledge = knowledge
+    def learn(self,acquired_knowledge):
+        self.knowledge.append(acquired_knowledge)
+        
